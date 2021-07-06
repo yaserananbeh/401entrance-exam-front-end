f9b74dfe0601b5daa936dc584e39d1d6804b79b2
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-07-06 10:42:13 +0300

    setup the react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+          Hello front end
+      </div>
+    )
+  }
+}
+
+export default App
